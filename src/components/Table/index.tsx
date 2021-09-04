9fbc8a993ad1e96979970e2f56f5f92c94219dab
Helgard Richard Ferreira <hrfer3@gmail.com>
2021-09-04 15:41:42 +0200

    style: Modify table styling

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -45,38 +45,47 @@ const Table: FunctionComponent = () => {
   }, []);
 
   return (
-    <div className="overflow-x-auto border-2 border-gray-300 rounded-lg">
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <Th>Name</Th>
-            <Th>Title</Th>
-            <Th>Email</Th>
-            <Th>City</Th>
-            <Th variant="action">
-              <span className="sr-only">Edit</span>
-            </Th>
-          </tr>
-        </thead>
-        <tbody>
-          {people.map((person, personIdx) => (
-            <tr
-              key={person.email}
-              className={personIdx % 2 === 0 ? "bg-white" : "bg-gray-50"}
-            >
-              <Td className="font-medium text-gray-900">{person.name}</Td>
-              <Td>{person.title}</Td>
-              <Td>{person.email}</Td>
-              <Td>{person.city}</Td>
-              <Td className="text-right font-medium">
-                <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                  Edit
-                </a>
-              </Td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="flex flex-col">
+      <div className="overflow-x-auto">
+        <div className="inline-block min-w-full">
+          <div className="overflow-hidden border border-gray-300 rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <Th>Name</Th>
+                  <Th>Title</Th>
+                  <Th>Email</Th>
+                  <Th>City</Th>
+                  <Th variant="action">
+                    <span className="sr-only">Edit</span>
+                  </Th>
+                </tr>
+              </thead>
+              <tbody>
+                {people.map((person, personIdx) => (
+                  <tr
+                    key={person.email}
+                    className={personIdx % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                  >
+                    <Td className="font-medium text-gray-900">{person.name}</Td>
+                    <Td>{person.title}</Td>
+                    <Td>{person.email}</Td>
+                    <Td>{person.city}</Td>
+                    <Td className="text-right font-medium">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Edit
+                      </a>
+                    </Td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
